26180a12bddac13ccd639a82e4efb1baeea7556d
Ian <hello@ian-patel.com>
2020-05-23 17:13:11 +1200

    Added source controller

---
+++ app/Http/Controllers/SourceController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Source;
+use Illuminate\Http\Request;
+
+class SourceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Source  $source
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Source $source)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Source  $source
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Source $source)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Source  $source
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Source $source)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Source  $source
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Source $source)
+    {
+        //
+    }
+}

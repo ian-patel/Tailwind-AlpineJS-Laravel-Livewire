f1f0e09229bd795224bb031434c3e406a2a9a20d
Ian <hello@ian-patel.com>
2020-05-27 00:13:24 +1200

    Added google oauth

--- config/services.php
+++ config/services.php
@@ -46,4 +46,10 @@ return [
         'redirect' => env('TWITTER_CLIENT_CALLBACK_URL'),
     ],
 
+    'google' => [
+        'client_id' => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect' => env('GOOGLE_CLIENT_CALLBACK_URL'),
+    ],
+
 ];

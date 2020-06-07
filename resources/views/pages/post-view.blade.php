27ce99fd3bc965f4a6ff74d3e26e3603311dfbc3
Ian <hello@ian-patel.com>
2020-06-08 00:09:10 +1200

    Added hacky breadcrumbs on post view page

--- resources/views/pages/post-view.blade.php
+++ resources/views/pages/post-view.blade.php
@@ -1,5 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
+<nav class="w-full p-3 m-4 font-sans rounded bg-grey-light">
+    <ol class="flex list-reset text-grey-dark">
+        <li><button onclick="goBack()" class="font-bold cursor-pointer text-blue">Posts</button></li>
+        <li><span class="mx-2">/</span></li>
+        <li>{{ $post->title }}</li>
+    </ol>
+</nav>
 <iframe src="{{ $post->link }}" class="w-full h-screen" frameborder="0"></iframe>
+
+<script>
+    function goBack() {
+        if (history.length > 2) {
+            window.history.back();
+        } else {
+            window.location.href = "/";
+        }
+    }
+</script>
 @overwrite

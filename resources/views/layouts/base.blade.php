10577a700e8ff45dc66f49cf1c66dd79f15bec1e
Ian <hello@ian-patel.com>
2020-06-13 11:52:59 +1200

    Added favicon

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -7,6 +7,7 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="icon" type="image/png" href="{{ url('favicon.png') }}">
 
     <title>{{ config('app.name', 'OneRead') }}</title>
     @livewireStyles
